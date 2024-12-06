42a098902e8cd0bc5fe66489128d2d572e1a192f
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2024-12-06 15:29:16 +0300

    Draw board, load and display sprites

--- src/main.c
+++ src/main.c
@@ -1,17 +1,21 @@
 #include "raylib.h"
+#include "raymath.h"
 
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 
-const int screenWidth = 1000;
-const int screenHeight = 800;
+#define UNPACK_VEC2(v) (v).x,(v).y
 
-// LOADING SCREEN
+const int screenWidth = 800;
+const int screenHeight = 1000;
+
 enum GameState {
-	GAME_STATE_LOAD_SCREEN,
+	GAME_STATE_LOAD,
 	GAME_STATE_PLAY,
 } state;
 
+// LOADING SCREEN
+const Color bgColor = RAYWHITE;
 const int buttonWidth = 300;
 const int buttonHeight = 100;
 bool play = false;
@@ -23,14 +27,33 @@ void UpdateLoadingScreen();
 void DrawLoadingScreen();
 // ------------------------------
 
+// GAME SCREEN
+const char *spriteFile = "assets/1024px-Chess_Pieces_Sprite.svg.png";
+const int cellSize = 80;
+const int numCells = 8;
+const int borderThickness = 10;
+const Color whiteCellColor = WHITE;
+/* const Color blackCellColor = BLACK; */
+const Color blackCellColor = BROWN;
+const Color borderColor = DARKBROWN;
+Texture2D spriteTexture = {0};
+
+void InitPlayScreen();
+void UpdatePlayScreen();
+void DrawPlayScreen();
+// ------------------------------
+
 int main(void)
 {
     // Initialization
     InitWindow(screenWidth, screenHeight, "Chess");
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 
-	state = GAME_STATE_LOAD_SCREEN;
-	InitLoadingScreen();
+	/* state = GAME_STATE_LOAD; */
+	/* InitLoadingScreen(); */
+
+	state = GAME_STATE_PLAY;
+	InitPlayScreen();
     //--------------------------------------------------------------------------------------
 
     // Main game loop
@@ -39,9 +62,12 @@ int main(void)
         // Update
         //----------------------------------------------------------------------------------
 		switch (state) {
-		case GAME_STATE_LOAD_SCREEN:
+		case GAME_STATE_LOAD:
 			UpdateLoadingScreen(); break;
+		case GAME_STATE_PLAY:
+		    UpdatePlayScreen(); break;
 		default:
+			TraceLog(LOG_ERROR, "State %d not handled for updating!", state);
 			return 1;
 		}
         //----------------------------------------------------------------------------------
@@ -50,12 +76,13 @@ int main(void)
         //----------------------------------------------------------------------------------
         BeginDrawing();
 		{			
-            ClearBackground(RAYWHITE);
-
 			switch (state) {
-			case GAME_STATE_LOAD_SCREEN:
+			case GAME_STATE_LOAD:
 				DrawLoadingScreen(); break;
+			case GAME_STATE_PLAY:
+				DrawPlayScreen(); break;
 			default:
+				TraceLog(LOG_ERROR, "State %d not handled for drawing!", state);
 				return 1;
 			}
         }
@@ -65,6 +92,7 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
+	UnloadTexture(spriteTexture);
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
@@ -75,25 +103,73 @@ void InitLoadingScreen()
 {
 	GuiSetStyle(DEFAULT, TEXT_SIZE, 36);
 	play = false;
+	bool fogOfWar = false;
+	bool multiplayer = false;
 }
 
 
 void UpdateLoadingScreen()
 {
 	if (play) {
-		// state = GAME_STATE_PLAY;
-		// InitGameState();
+		state = GAME_STATE_PLAY;
+		InitPlayScreen();
 	}
 }
 
 void DrawLoadingScreen()
 {
+	ClearBackground(bgColor);
+
 	const char *titleText = "Chess!";
 	const int titleSize = 50;
 	int textWidth = MeasureText(titleText, titleSize);
+
 	DrawText(titleText, (screenWidth-textWidth)/2, (screenHeight-3*buttonHeight-titleSize)/2, titleSize, BLACK);
 	if (GuiButton((Rectangle){(screenWidth-buttonWidth)/2, (screenHeight-buttonHeight)/2, buttonWidth, buttonHeight}, "Play!"))
 	    play = true;
 	GuiCheckBox((Rectangle){(screenWidth-buttonWidth)/2, (screenHeight+buttonWidth/2)/2, buttonWidth/4, buttonHeight/2}, "Fog of War", &fogOfWar);
 	GuiCheckBox((Rectangle){(screenWidth-buttonWidth)/2, (screenHeight+buttonWidth)/2, buttonWidth/4, buttonHeight/2}, "Multiplayer", &multiplayer);
+}
+
+void InitPlayScreen()
+{
+	Image spriteImg = LoadImage(spriteFile);
+	ImageResize(&spriteImg, 6*cellSize, 2*cellSize);
+	spriteTexture = LoadTextureFromImage(spriteImg);
+	UnloadImage(spriteImg);
+}
+
+void UpdatePlayScreen()
+{
+}
+
+void DrawPlayScreen()
+{
+	ClearBackground(bgColor);
+
+	const Vector2 screenSize = {screenWidth, screenHeight};
+	const Vector2 cellSizeV = {cellSize, cellSize};
+
+	const Vector2 offset = Vector2Scale(Vector2Subtract(screenSize, Vector2Scale(cellSizeV, numCells)), 0.5f);
+
+	// Draw Border
+	DrawRectangleLinesEx((Rectangle){offset.x - borderThickness,offset.y - borderThickness,
+									 numCells*cellSize + 2*borderThickness, numCells*cellSize + 2*borderThickness},
+	    borderThickness, borderColor);
+
+	// Draw grids
+	for (int i=0; i<numCells; i++) {
+		for (int j=0; j<numCells; j++) {
+			Color color = ((i + j) % 2) ? blackCellColor : whiteCellColor;
+			Vector2 pos = Vector2Add(offset, Vector2Multiply(cellSizeV, (Vector2){i,j}));
+			DrawRectangleV(pos, cellSizeV, color);
+			if (j < 4)
+				DrawTextureRec(spriteTexture, (Rectangle){0, 0, cellSize, cellSize}, pos, WHITE);
+			else
+				DrawTextureRec(spriteTexture, (Rectangle){0, cellSize, cellSize, cellSize}, pos, WHITE);
+		}
+	}
+
+	// Draw Pieces
+	/* DrawTextureRec(Texture2D texture, Rectangle source, Vector2 position, Color tint) */
 }
