53887e9ee37a71a5f9c179efa855f9d36544c010
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2024-12-11 18:27:59 +0300

    Implement pawn diagonal capturing

--- src/piecemovement.c
+++ src/piecemovement.c
@@ -10,18 +10,31 @@ bool isValidCell(int x, int y)
 	return x >= 0 && y >= 0 && x < 8 && y < 8;
 }
 
+static void addCandidate(Cell *candidates[], int *num_candidates, Cell *newCandidate)
+{
+	candidates[*num_candidates] = newCandidate;
+	(*num_candidates)++;
+}
+
 void getPawnCandidates(Board *board, int x, int y, Cell *candidates[], int *num_candidates)
 {
 	Player player = board->cells[y][x].player;
 	int direction = (player == PLAYER_WHITE) ? -1 : 1;
-	// TODO: for loop instead of this
-	// check 1 step forward
 	int y_forward = y+direction;
+
+	// check diagonals
+	// TODO: implement En passant
+	for (int x_diag=x-1; x_diag<=x+1; x_diag+=2) {
+		if (isValidCell(x_diag, y_forward) && board->cells[y_forward][x_diag].player == otherPlayer(player)) {
+			addCandidate(candidates, num_candidates, &board->cells[y_forward][x_diag]);
+		}
+	}
+
+	// check 1 step forward
 	if (!isValidCell(x, y_forward) || board->cells[y_forward][x].player != PLAYER_NONE) {
 		return;
 	}
-	candidates[*num_candidates] = &board->cells[y_forward][x];
-	(*num_candidates)++;
+	addCandidate(candidates, num_candidates, &board->cells[y_forward][x]);
 
 	// check 2 steps forward if pawn hasn't moved yet
 	y_forward = y+2*direction;
@@ -29,6 +42,5 @@ void getPawnCandidates(Board *board, int x, int y, Cell *candidates[], int *num_
 	if (y != y_base || !isValidCell(x, y_forward) || board->cells[y_forward][x].player != PLAYER_NONE) {
 		return;
 	}
-	candidates[*num_candidates] = &board->cells[y_forward][x];
-	(*num_candidates)++;
+	addCandidate(candidates, num_candidates, &board->cells[y_forward][x]);
 }
